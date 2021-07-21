8fc5c6c5bc14b3f25d710368563dde589d08bdd1
Karen <Zakharyan.kar@gmail.com>
2021-07-22 02:34:03 +0400

    feat: add shit code...

--- 3.Form-app/src/components/Form.jsx
+++ 3.Form-app/src/components/Form.jsx
@@ -5,25 +5,46 @@ class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      nameValid: '',
+      name: {
+        value: '',
+        validName: '',
+      },
+      surname: {
+        value: '',
+        validSurname: '',
+      },
+      email: {
+        value: '',
+        validEmail: '',
+      },
+      pass: {
+        value: '',
+        validPass: '',
+      },
+      passRepeat: {
+        value: '',
+        validPassRepeat: '',
+      },
     };
 
-    this.validate = this.validate.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.mouseLeave = this.mouseLeave.bind(this);
   }
   
-  validate(e) {
-    const regex = /^[a-zA-Z ]{3,16}$/;
-    
-    this.setState({
-      nameValid: !regex.test(e.target.value) ? 'Enter valid name' : '',
-    })
+  handleChange(e) {
+    const nameValidator = /^[a-zA-Z]{3,16}$/;
+    const emailValidator =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    console.log(e.target.value);
+    this.setState((state) => ({
+      state[e.target.name].value : e.target.value
+    }))
   }
 
   mouseLeave(e) {
     if(e.target.value === '') {
       this.setState({
-        nameValid: 'Enter your name',
+        validName: 'Enter your name',
       })
     } 
   }
@@ -43,16 +64,16 @@ class Form extends React.Component {
                 </label>
               </div>
               <div className='input-group'>
-                <input onChange={this.validate} onBlur={this.mouseLeave} className='input' name='Name' type="text" placeholder="Name" />
-                <p>{this.state.nameValid}</p>
-                <input className='input' name='Surname' type="text" placeholder="Surname" />
-                <p></p>
-                <input className='input' name='Email' type="text" placeholder="Email" />
-                <p></p>
-                <input className='input' name='password' type="text" placeholder="Password" />
-                <p></p>
-                <input className='input' name='password' type="text" placeholder="Repeat password" />
-                <p></p>
+                <input onChange={this.handleChange} onBlur={this.mouseLeave} className='input' name='name' type="text" placeholder="Name" />
+                <p>{this.state.name.validName}</p>
+                <input onChange={this.handleChange} onBlur={this.mouseLeave} className='input' name='surname' type="text" placeholder="Surname" />
+                <p>{this.state.validSurname}</p>
+                <input onChange={this.handleChange} onBlur={this.mouseLeave} className='input' name='email' type="text" placeholder="Email" />
+                <p>{this.state.validEmail}</p>
+                <input onChange={this.handleChange} onBlur={this.mouseLeave} className='input' name='pass' type="text" placeholder="Password" />
+                <p>{this.state.validPass}</p>
+                <input onChange={this.handleChange} onBlur={this.mouseLeave} className='input' name='passRepeat' type="text" placeholder="Repeat password" />
+                <p>{this.state.validPassRepeat}</p>
               </div>
               <div>
                 <button className='submit-btn' type='submit'>
